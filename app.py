a50e811762d33f180ec65b878610a365e97bb87d
Hendra Manudinata <manoedinata@gmail.com>
2023-01-10 07:20:25 -0500

    Initial commit: Initial app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+session = requests.Session()
+URL = "https://data.asmaulhusna.manoe.my.id"
+
+@app.route("/")
+def home():
+    req = session.get(URL)
+    data = req.json()
+
+    return render_template("home.html", data=data)
+
+@app.route("/random")
+def show_random():
+    req = session.get(URL + "/random")
+    data = req.json()
+
+    return render_template("random.html", data=data)
+
+if __name__ == "__main__":
+    app.run()
